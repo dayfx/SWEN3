408020a12bb0a38751ea7e46d1956ef10956e70f
Andy Mihalca <if23b300@technikum-wien.at>
2025-09-11 16:57:16 +0200

    initial commit

---
+++ src/test/java/com/fhtechnikum/swen3/Swen3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhtechnikum.swen3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swen3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
