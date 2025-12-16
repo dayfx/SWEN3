6e8596ac84903cd6a638a5f2e2ee5743ae6bd60a
dayfx <if22b206@technikum-wien.at>
2025-12-16 16:57:31 +0100

    pipeline fix?

--- PaperlessServices/src/test/java/com/fhtechnikum/paperlessservices/PaperlessServicesApplicationTests.java
+++ PaperlessServices/src/test/java/com/fhtechnikum/paperlessservices/PaperlessServicesApplicationTests.java
@@ -1,12 +1,23 @@
 package com.fhtechnikum.paperlessservices;
 
+import com.fhtechnikum.paperlessservices.persistence.repository.DocumentRepository;
+import com.fhtechnikum.paperlessservices.persistence.repository.ElasticSearchRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
 class PaperlessServicesApplicationTests {
+
+    // 1. Mock Postgres (So it doesn't crash if DB is missing)
+    @MockitoBean
+    private DocumentRepository documentRepository;
+
+    // 2. Mock Elasticsearch (So it doesn't crash looking for Port 9200)
+    @MockitoBean
+    private ElasticSearchRepository elasticSearchRepository;
 
     @Test
     void contextLoads() {
